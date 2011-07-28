a2955ccb253e62d834213f019e0b8a3e8837ca5a
Jacob Buck <buck.jacob@gmail.com>
2011-07-29 10:45:28 +1200

    Imported new Settings page template.

--- settings.php
+++ settings.php
@@ -15,6 +15,165 @@
 	<?php endif; ?>
 	
 	<form action="" method="post" accept-charset="utf-8">
+		<div class="metabox-holder has-right-sidebar">
+			<div class="inner-sidebar">
+				<div class="stuffbox">
+					<h3>Settings</h3>
+					<div class="submitbox">
+						<div id="major-publishing-actions">
+							<div id="delete-action">
+								<input name="revert" type="submit" class="submitdelete deletion" value="Revert to defaults">
+							</div>
+							<div id="publishing-action">
+								<input name="save" type="submit" class="button-primary" value="Save Changes">
+							</div>
+							<div class="clear"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="post-body">
+				<div id="post-body-content">
+				
+					<div class="stuffbox">
+						<h3><label><input type="checkbox" name="" class="checkbox"><span>Posts</span></label></h3>
+						<div class="inside">
+							
+							<p><b>Rename</b></p>
+						
+							<ul class="fields">
+								<li><label><span>Name</span> <input type="text" name="" value="" placeholder="Posts" class="textbox"></label></li>
+								<li><label><span>Singular</span> <input type="text" name="" value="" placeholder="Post" class="textbox"></label></li>
+							</ul>
+							
+							<p><b>Supports</b></p>
+						
+							<ul class="fields">
+								<li><label><input type="checkbox" name="" class="checkbox"> Title</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Editor</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Author</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Excerpt</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Trackbacks</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Custom Fields</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Comments</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Revisions</label></li>
+							</ul>
+							
+							<p><b>Taxonomies</b></p>
+						
+							<ul class="fields">
+								<li><label><input type="checkbox" name="" class="checkbox"> Categories</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Post Tags</label></li>
+							</ul>
+						
+						</div>
+					</div>
+				
+					<div class="stuffbox">
+						<h3><label><input type="checkbox" name="" class="checkbox"><span>Links</span></label></h3>
+						<div class="inside">
+						</div>
+					</div>
+				
+					<div class="stuffbox">
+						<h3><label><input type="checkbox" name="" class="checkbox"><span>Pages</span></label></h3>
+						<div class="inside">
+						
+							<p><b>Supports</b></p>
+						
+							<ul class="fields">
+								<li><label><input type="checkbox" name="" class="checkbox"> Title</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Editor</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Author</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Excerpt</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Trackbacks</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Custom Fields</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Comments</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Revisions</label></li>
+							</ul>
+						
+						</div>
+					</div>
+				
+					<div class="stuffbox">
+						<h3><label><input type="checkbox" name="" class="checkbox"><span>Comments</span></label></h3>
+						<div class="inside">
+						</div>
+					</div>
+				
+					<div class="stuffbox">
+						<h3><span>Site</span></h3>
+						<div class="inside">
+						
+							<p><b>Remove Head Tags</b></p>
+						
+							<ul class="fields">
+								<li><label><input type="checkbox" name="" class="checkbox"> Remote Publishing</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> RSS Feeds</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Posts rel links</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Generator</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> l190n.js</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Admin Bar Margin</label></li>
+							</ul>
+																		
+						</div>
+					</div>
+									
+					<div class="stuffbox">
+						<h3><span>Administraton</span></h3>
+						<div class="inside">
+						
+							<p><b>Dashboard</b></p>
+						
+							<ul class="fields">
+								<li><label><input type="checkbox" name="" class="checkbox"> Right Now</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Recent Comments</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Incoming Links</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Plugins</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> QuickPress</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Recent Drafts</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> WordPress Blog</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Other WordPress News</label></li>
+							</ul>
+												
+							<p><b>Menu</b></p>
+						
+							<ul class="fields">
+								<li><label><input type="checkbox" name="" class="checkbox"> Pages before Posts</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Hide Plugins</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Hide Tools</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Hide Editors</label></li>
+							</ul>
+						
+							<p><b>Roles</b></p>
+						
+							<ul class="fields">
+								<li><label><input type="checkbox" name="" class="checkbox"> Subscriber</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Contributor</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Author</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Editor</label></li>
+							</ul>
+												
+							<p><b>Advanced</b></p>
+						
+							<ul class="fields">
+								<li><label><input type="checkbox" name="" class="checkbox"> Disable Admin Bar</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Disable Update Checks</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Force Flash Uploader</label></li>
+								<li><label><input type="checkbox" name="" class="checkbox"> Strict TinyMCE Pasting</label></li>
+							</ul>
+												
+						</div>
+					</div>
+				
+				</div>
+			</div>
+		</div>
+	</form>
+	
+	<h2>Old Form</h2>
+	
+	<form action="" method="post" accept-charset="utf-8">
 		<table class="form-table">
 			<tbody>
 				<tr valign="top">
